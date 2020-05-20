701fe1b6820f8e6873bfedcddf20499749e57c99
Weston Walker <walk8919@gmail.com>
2020-05-19 23:22:41 -0500

    formatting update.

--- BuckleUp/functions.php
+++ BuckleUp/functions.php
@@ -3,14 +3,15 @@
 function loadCss() {
   wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css');
 }
-add_action('wp_enqueue_scripts', 'loadCss');
 
 function loadJavascript() {
   wp_deregister_script('jquery');
-	wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', array(), null, true);
+  wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', array(), null, true);
   wp_enqueue_script('popperjs', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array('jquery'), false, true);
   wp_enqueue_script('bootstrapjs', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js', array('jquery'), false, true);
 }
+
+add_action('wp_enqueue_scripts', 'loadCss');
 add_action('wp_enqueue_scripts', 'loadJavascript');
 
 ?>
